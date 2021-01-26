8da14cc550880cad66abe4b175efbe8b5953691a
Admin <alex.r@ramotion.agency>
2021-01-26 19:39:46 +0300

    added 5th home work TODO: make dropdown nav menu

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {PATH} from "./Routes";
+import styles from "./Header.module.css"
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={styles.aside}>
+            <Link to={PATH.PRE_JUNIOR}>Pre Junior</Link>
+            <Link to={PATH.JUNIOR}>Junior</Link>
+            <Link to={PATH.JUNIOR_PLUS}>Junior Plus</Link>
         </div>
     );
 }
