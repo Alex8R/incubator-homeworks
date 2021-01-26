6ef9458b7cd5827954fa3cf21ccf9b202d8f43d6
AlexR <alex.r@ramotion.agency>
2021-01-26 20:04:16 +0300

    Updates

---
+++ precache-manifest.2b9c4d302bf63b97253b379d065c5284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529e8e8c6f85b841d80407f4859f3324",
+    "url": "/Alex8R/incubator-homeworks/index.html"
+  },
+  {
+    "revision": "9d4770429a70ac4315c1",
+    "url": "/Alex8R/incubator-homeworks/static/css/main.d27563aa.chunk.css"
+  },
+  {
+    "revision": "052fd607903bb32283c5",
+    "url": "/Alex8R/incubator-homeworks/static/js/2.9274fcc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Alex8R/incubator-homeworks/static/js/2.9274fcc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4770429a70ac4315c1",
+    "url": "/Alex8R/incubator-homeworks/static/js/main.8658c63a.chunk.js"
+  },
+  {
+    "revision": "c63cd935cbd2a8c5416a",
+    "url": "/Alex8R/incubator-homeworks/static/js/runtime-main.21850747.js"
+  }
+]);
